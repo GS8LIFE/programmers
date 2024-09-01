841e4039fcf9bf2f56449dbd0d7848d564c8da6d
GS8LIFE <gs.8life@gmail.com>
2024-09-01 18:19:38 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int solution(int num_list[], size_t num_list_len) {
+    int answer = 0;
+    int Checker = 0;
+    if (num_list_len < 11)
+    {
+        int temp = 1;
+        for (size_t i = 0; i < num_list_len; i++)
+        {
+            temp *= num_list[i];
+            answer = temp;
+        }
+    }
+    if (num_list_len >= 11)
+    {
+        for (size_t i = 0; i < num_list_len; i++)
+        {
+            answer += num_list[i];
+        }
+    }
+    return answer;
+}
